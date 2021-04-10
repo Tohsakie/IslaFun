fbec0815c1adf1e94c8a4c20a66ae18e5b34b5e8
Théo Fontana <fontana.theo.me@gmail.com>
2021-04-10 14:34:12 +0200

    utilisation de rowAdapter

--- fontanaThacquarG/app/src/main/java/com/example/fontanathacquarg/ExerciceMultiplyActivity.java
+++ fontanaThacquarG/app/src/main/java/com/example/fontanathacquarg/ExerciceMultiplyActivity.java
@@ -3,7 +3,9 @@ package com.example.fontanathacquarg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -11,21 +13,28 @@ import java.util.ArrayList;
 public class ExerciceMultiplyActivity extends AppCompatActivity {
 
     public static int MATH_KEY = 0;
+    private RowAdapter rowAdapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercice_multiply);
         MATH_KEY = getIntent().getExtras().getInt("laTable");
-        ArrayList<String> multiplicationArray = new ArrayList<>();
+        ArrayList<Row> multiplicationArray = new ArrayList<>();
         String tempStr;
+
         for(int i = 0; i <= 10; i++){
             tempStr = MATH_KEY + " x " + i + " = ";
-            multiplicationArray.add(tempStr);
+            multiplicationArray.add(new Row(tempStr));
+
         }
 
-        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.activity_exercice_multiply_row, multiplicationArray);
+        rowAdapter = new RowAdapter(this, R.layout.activity_exercice_multiply_row, multiplicationArray);
         ListView listView = findViewById(R.id.listView);
-        listView.setAdapter(adapter);
+        listView.setAdapter(rowAdapter);
     }
+
+
+
 }
