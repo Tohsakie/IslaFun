c8b716c0d58d539586a46ac9890823ac86d6e065
Théo Fontana <fontana.theo.me@gmail.com>
2021-04-12 02:38:47 +0200

    un debut de compte

--- fontanaThacquarG/app/src/main/java/com/example/fontanathacquarg/AddUserActivity.java
+++ fontanaThacquarG/app/src/main/java/com/example/fontanathacquarg/AddUserActivity.java
@@ -1,5 +1,6 @@
 package com.example.fontanathacquarg;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,76 +9,105 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class AddUserActivity extends AppCompatActivity {
     private UserAdapter adapter;
+    private DatabaseClient mdb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mdb = DatabaseClient.getInstance(getApplicationContext());
         setContentView(R.layout.activity_add_user);
-        DatabaseClient mdb = DatabaseClient.getInstance(getApplicationContext());
 
         Button button = findViewById(R.id.valider);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nom = ((EditText) findViewById(R.id.nom)).getText().toString();
-                String prenom = ((EditText) findViewById(R.id.prenom)).getText().toString();
-                mdb.getAppDatabase().userDao().insert(new User(nom, prenom));
-                System.out.println("############################");
-                System.out.println(getUsers());
-                System.out.println("############################");
+                saveUser();
+                finish();
             }
         });
     }
+    private void saveUser() {
 
-        private void getUsers(){
-            ///////////////////////
-            // Classe asynchrone permettant de récupérer des taches et de mettre à jour le listView de l'activité
-            class GetUsers extends AsyncTask<Void, Void, List<User>> {
+        // Récupérer les informations contenues dans les vues
+        final String nom = ((EditText) findViewById(R.id.nom)).getText().toString();
+        final String prenom = ((EditText) findViewById(R.id.prenom)).getText().toString();
 
-                @Override
-                protected List<User> doInBackground(Void... voids) {
-                    List<User> userList = mdb.getAppDatabase()
-                            .userDao()
-                            .getAll();
-                    return userList;
-                }
-
-                @Override
-                protected void onPostExecute(List<User> tasks) {
-                    super.onPostExecute(tasks);
-
-                    // Mettre à jour l'adapter avec la liste de taches
-                    adapter.clear();
-                    adapter.addAll(tasks);
-
-                    // Now, notify the adapter of the change in source
-                    adapter.notifyDataSetChanged();
-                }
-            }
-
-            //////////////////////////
-            // IMPORTANT bien penser à executer la demande asynchrone
-            // Création d'un objet de type GetTasks et execution de la demande asynchrone
-            GetUsers gu = new GetUsers();
-            gu.execute();
+        // Vérifier les informations fournies par l'utilisateur
+        if (nom.isEmpty()) {
+            EditText editTextNom = findViewById(R.id.nom);
+            editTextNom.setError("nom required");
+            editTextNom.requestFocus();
+            return;
         }
 
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
+        if (prenom.isEmpty()) {
+            EditText editTextPrenom = findViewById(R.id.prenom);
+            editTextPrenom.setError("prenom required");
+            editTextPrenom.requestFocus();
+            return;
+        }
 
-            if (requestCode == REQUEST_CODE_ADD && resultCode == RESULT_OK) {
+        /**
+         * Création d'une classe asynchrone pour sauvegarder la tache donnée par l'utilisateur
+         */
 
-                // Mise à jour des taches
-                getTasks();
+        class SaveUser extends AsyncTask<Void, Void, User> {
+
+            @Override
+            protected User doInBackground(Void... voids) {
+
+                // creating a task
+                User user = new User(nom, prenom);
+
+                // adding to database
+//                long id = mdb.getAppDatabase()
+//                        .userDao()
+//                        .insert(user);
+
+                    mdb.getAppDatabase()
+                        .userDao()
+                        .insert(user);
+
+                // mettre à jour l'id de la tache
+                // Nécessaire si on souhaite avoir accès à l'id plus tard dans l'activité
+//                user.setId(id);
+
+
+                return user;
+            }
+
+            @Override
+            protected void onPostExecute(User user) {
+                super.onPostExecute(user);
+
+                // Quand la tache est créée, on arrête l'activité AddTaskActivity (on l'enleve de la pile d'activités)
+                setResult(RESULT_OK);
+                finish();
+                Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
             }
         }
+
+        //////////////////////////
+        // IMPORTANT bien penser à executer la demande asynchrone
+        SaveUser st = new SaveUser();
+        st.execute();
+    }
+//        @Override
+//        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//            super.onActivityResult(requestCode, resultCode, data);
+//
+//            if (requestCode == REQUEST_CODE_ADD && resultCode == RESULT_OK) {
+//
+//                // Mise à jour des taches
+//                getUsers();
+//            }
+//        }
 
 
     }
-
-}
--- fontanaThacquarG/app/src/main/java/com/example/fontanathacquarg/MainActivity.java
+++ fontanaThacquarG/app/src/main/java/com/example/fontanathacquarg/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.fontanathacquarg;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private UserAdapter adapter;
+    private DatabaseClient mdb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.mainListView);
+        adapter = new UserAdapter(this, new ArrayList<User>());
+        listView.setAdapter(adapter);
+        mdb = DatabaseClient.getInstance(getApplicationContext());;
+
     }
 
     public void onAno(View view){
@@ -22,5 +35,58 @@ public class MainActivity extends AppCompatActivity {
     public void onPlus(View view){
         Intent ExoViewActivityIntent = new Intent(MainActivity.this, AddUserActivity.class);
         startActivity(ExoViewActivityIntent);
+    }
+
+    private void getUsers() {
+        ///////////////////////
+        // Classe asynchrone permettant de récupérer des taches et de mettre à jour le listView de l'activité
+        class GetUser extends AsyncTask<Void, Void, List<User>> {
+
+            @Override
+            protected List<User> doInBackground(Void... voids) {
+                List<User> userList = mdb.getAppDatabase()
+                        .userDao()
+                        .getAll();
+                return userList;
+            }
+
+            @Override
+            protected void onPostExecute(List<User> users) {
+                super.onPostExecute(users);
+
+                // Mettre à jour l'adapter avec la liste de taches
+                adapter.clear();
+                adapter.addAll(users);
+
+                // Now, notify the adapter of the change in source
+                adapter.notifyDataSetChanged();
+            }
+        }
+
+        //////////////////////////
+        // IMPORTANT bien penser à executer la demande asynchrone
+        // Création d'un objet de type GetTasks et execution de la demande asynchrone
+        GetUser gt = new GetUser();
+        gt.execute();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+
+            // Mise à jour des taches
+            getUsers();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Mise à jour des taches
+        getUsers();
+
     }
 }
