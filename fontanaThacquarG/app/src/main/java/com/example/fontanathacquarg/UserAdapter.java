a799d02fd6d0e8c7a56055c110198560f300f184
Théo Fontana <fontana.theo.me@gmail.com>
2021-04-12 03:42:25 +0200

    fix accueil

--- fontanaThacquarG/app/src/main/java/com/example/fontanathacquarg/UserAdapter.java
+++ fontanaThacquarG/app/src/main/java/com/example/fontanathacquarg/UserAdapter.java
@@ -39,7 +39,6 @@ public class UserAdapter extends ArrayAdapter<User> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        // Récupération de la multiplication
         final User user = getItem(position);
 
         // Charge le template XML
@@ -52,10 +51,7 @@ public class UserAdapter extends ArrayAdapter<User> {
         // Récupération des objets graphiques dans le template
         TextView nomPrenom = rowView.findViewById(R.id.nomPrenom);
 
-        String nomStr = getUserById(id);
-        String prenomStr = mdb.getAppDatabase().userDao().findUserById(id++).getPrenom();
-        nomPrenom.setText(nomStr + " " + prenomStr);
-        nomPrenom.setText("hahaha");
+        nomPrenom.setText(user.getNom() + " " + user.getPrenom());
 
 //
 //        //
@@ -65,39 +61,5 @@ public class UserAdapter extends ArrayAdapter<User> {
         //
         return rowView;
     }
-
-    private void getUserById(int id) {
-        ///////////////////////
-        // Classe asynchrone permettant de récupérer des taches et de mettre à jour le listView de l'activité
-        class GetUser extends AsyncTask<Void, Void, List<User>> {
-
-            @Override
-             List<User> doInBackground(Void... voids) {
-                List<User> userList = (List<User>) mdb.getAppDatabase()
-                        .userDao().findUserById(UserAdapter.id);
-                return userList;
-            }//cast to list pour rien detruire, recuperer .get(0)
-
-            @Override
-            protected void onPostExecute(List<User> users) {
-                super.onPostExecute(users);
-
-                // Mettre à jour l'adapter avec la liste de taches
-                adapter.clear();
-                adapter.addAll(users);
-
-                // Now, notify the adapter of the change in source
-                adapter.notifyDataSetChanged();
-            }
-        }
-
-        //////////////////////////
-        // IMPORTANT bien penser à executer la demande asynchrone
-        // Création d'un objet de type GetTasks et execution de la demande asynchrone
-        GetUser gt = new GetUser();
-        gt.execute();
-        return
-    }
-
 
 }
